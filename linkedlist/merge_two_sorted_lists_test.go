23ab7380821474f9e90175aa2e3d76014284e0b2
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2024-06-23 06:45:10 +0600

    understand pointers

--- linkedlist/merge_two_sorted_lists_test.go
+++ linkedlist/merge_two_sorted_lists_test.go
@@ -10,4 +10,17 @@ func TestMergeTwoLists(t *testing.T) {
 	exmp1List2 := &ListNode{1, &ListNode{3, &ListNode{4, &ListNode{5, nil}}}}
 	head := mergeTwoLists(exmp1List1, exmp1List2)
 	fmt.Println(head)
+
+	headItem := &ListNode{1, nil}
+	var curItem *ListNode
+	item1 := &ListNode{2, &ListNode{3, &ListNode{4, nil}}}
+	fmt.Println(headItem, curItem, item1)
+
+	curItem = headItem
+	curItem.Next = item1
+	fmt.Printf("%+v | %+v | %+v\n", headItem, curItem, item1)
+
+	curItem = curItem.Next
+	curItem.Next = item1.Next
+	fmt.Printf("%+v | %+v | %+v\n", headItem, curItem, item1)
 }
